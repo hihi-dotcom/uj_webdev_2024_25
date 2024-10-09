ac00c44599327247266342bc0d6d537f86610ba7
Adam Czirjak <124435921+hihi-dotcom@users.noreply.github.com>
2024-10-09 20:46:03 +0200

    Calculator with good code and wonderful comments as well.

--- hazi_feladat_szamologep/index2.js
+++ hazi_feladat_szamologep/index2.js
@@ -1,19 +1,19 @@
-var kijelzo  = document.getElementById("kijelzo"); 
+var kijelzo  = document.getElementById("kijelzo"); //a kijelző nevű változót deklaráljuk, mely alapvetően a számológép kijelzőét szimbolizálja.
 
 function appendToDisplay(input){ 
-    kijelzo.value += input; 
+    kijelzo.value += input;  // Eme függvény segítségével jön létre a lehetőség arra,hogy a számokat megjelenítsük a számológép kijelzőjén.
 }
 
 function torolj(){
-    kijelzo.value = "";
+    kijelzo.value = ""; // Ez a függvény egy üres sztringgel tér vissza,tehát kitörli a kijelzőn lévő összes számot. Ezt term. külön gombhoz kapcsoltam.
 }
 
 function szamolj(){
-  if (kijelzo.value.includes('^2')) {
+  if (kijelzo.value.includes('^2')) { //A számológép "törzse", először egy elágazással, mely a négyzetre emelés elvégzéshez szükséges. Melyhez hozzá adtunk egy try-catch szerkezetet a esetlegesen jelentkező hibák kezelésére. Mindenkét helyen a JS beépített függvényeit használtam ezek a:"pow", "eval", voltak. Természetesen a hibakezelést mindkét helyen alkalmaztam.
     kijelzo.value = parseInt(kijelzo.value);
    
     try {
-      kijelzo.value = Math.pow(kijelzo.value, 2); 
+      kijelzo.value = Math.pow(kijelzo.value, 2); //Apró módosítás/ szépítésként a négyzetre emelés gomb függvényének bemeneti értékeként csak a ^2-t adtam meg,hogy ne jelezze ki csúnyán a számológép kijelzőjén, a művelet elvégzésekor, ezért fentebb az elágazásban is ezt alkalmaztam.
     } catch (e) {
       kijelzo.value = "Error";
     }
